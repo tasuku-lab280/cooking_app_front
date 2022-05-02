291c0560016be0022b80dbad16f7f69140096c6a
tasuku-lab280 <loweng280@gmail.com>
2022-05-02 23:08:25 +0900

    feat: レシピ検索

--- src/pages/recipes.tsx
+++ src/pages/recipes.tsx
@@ -1,16 +1,25 @@
 import type { NextPage } from 'next';
 import { gql, useQuery } from '@apollo/client';
-import { Grid } from '@mantine/core';
+import {
+  Grid,
+  MultiSelect,
+  Loader,
+  TextInput,
+  Text,
+  Center,
+  Button,
+} from '@mantine/core';
+import { useForm } from '@mantine/form';
 
 import { RecipeItem } from 'components/RecipeItem';
-import { SkeletonItem } from 'components/RecipeItem/SkeltonItem';
+import { useCategory } from 'hooks/useCategory';
 import { BaseLayout } from 'layouts/BaseLayout';
 import { RecipesQuery } from 'services/graphql/types/generated';
 import { withCurrentUser } from 'utils/withCurrentUser';
 
 const RECIPES_QUERY = gql`
-  query RecipesQuery {
-    recipes {
+  query RecipesQuery($keyword: String, $categoryIds: [String!]) {
+    recipes(keyword: $keyword, categoryIds: $categoryIds) {
       id
       name
       picture
@@ -31,21 +40,86 @@ const RECIPES_QUERY = gql`
 `;
 
 const Recipes: NextPage = () => {
-  const { data, loading } = useQuery<RecipesQuery>(RECIPES_QUERY);
+  const {
+    data: recipesData,
+    loading: recipesLoading,
+    refetch,
+  } = useQuery<RecipesQuery>(RECIPES_QUERY, {
+    variables: { keyword: '', categoryIds: [] },
+    notifyOnNetworkStatusChange: true,
+  });
+
+  const { data: categoryData, loading: categoryLoading } = useCategory();
+  const categoryList =
+    categoryLoading || !categoryData
+      ? []
+      : categoryData.categories.map((c) => {
+          return { value: c.id, label: c.name };
+        });
+
+  const form = useForm({
+    initialValues: {
+      keyword: '',
+      categoryIds: [],
+    },
+  });
+
+  const refetchRecipes = () => {
+    const { keyword, categoryIds } = form.values;
+    refetch({ keyword, categoryIds });
+  };
 
   return (
     <BaseLayout>
-      <Grid>
-        {loading &&
-          [...Array(5)].map((_, index) => <SkeletonItem key={index} />)}
-        {data && (
-          <>
-            {data.recipes.map((recipe) => (
-              <RecipeItem key={recipe.id} recipe={recipe} />
-            ))}
-          </>
-        )}
+      <Grid align="flex-end" mb={25}>
+        <Grid.Col lg={5}>
+          <TextInput
+            placeholder="レシピ名で検索"
+            {...form.getInputProps('keyword')}
+          />
+        </Grid.Col>
+        <Grid.Col lg={5}>
+          <MultiSelect
+            data={categoryList}
+            placeholder="カテゴリで検索"
+            searchable
+            clearable
+            maxSelectedValues={3}
+            {...form.getInputProps('categoryIds')}
+          />
+        </Grid.Col>
+        <Grid.Col lg={2}>
+          <Button
+            color="teal"
+            disabled={recipesLoading}
+            onClick={refetchRecipes}
+          >
+            検索
+          </Button>
+        </Grid.Col>
       </Grid>
+      {recipesLoading && (
+        <Center>
+          <Loader color="teal" size="lg" />
+        </Center>
+      )}
+      {!recipesLoading && recipesData && (
+        <>
+          {recipesData.recipes.length === 0 ? (
+            <Center>
+              <Text weight={800} size="xl">
+                レシピが見つかりませんでした
+              </Text>
+            </Center>
+          ) : (
+            <Grid>
+              {recipesData.recipes.map((recipe) => (
+                <RecipeItem key={recipe.id} recipe={recipe} />
+              ))}
+            </Grid>
+          )}
+        </>
+      )}
     </BaseLayout>
   );
 };
